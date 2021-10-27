b6802d1142d7103ccaec62c13c099a4a8e60b00e
Rzeszowski17 <70712364+Rzeszowski17@users.noreply.github.com>
2021-10-27 22:13:21 +0200

    Add files via upload

---
+++ Bazy_danych_2021-2022/21.10.26/airport1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Airport</title>
+</head>
+<body>
+<div id="BANER">
+TABELA ODLOTÓW W DNIU 12 kwietnia 2017r.
+</div>
+<div id="BLOKLEWY">
+    
+</div>
+<div id="BLOKPRAWY">
+</div>
+</body>
+</html>
